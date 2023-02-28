9ef5c06ee66da5975a2244a92e07e4dd1a8b4f2c
toseth97 <oluwaseun9721@gmail.com>
2023-02-28 15:33:56 +0100

    Frontpage file created.

---
+++ frontend/intellauto/src/components/Frontpage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navigation from "./Navigation";
+
+const Frontpage = () => {
+    return (
+        <section className="flex frontpage ">
+            <Navigation />
+        </section>
+    );
+};
+
+export default Frontpage;
