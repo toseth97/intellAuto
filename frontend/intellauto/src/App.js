f369ddb38d7d4f978b357b99e371b97a2197d7eb
toseth97 <oluwaseun9721@gmail.com>
2023-02-28 15:37:22 +0100

    frontpage file added

--- frontend/intellauto/src/App.js
+++ frontend/intellauto/src/App.js
@@ -1,10 +1,10 @@
-import Navigation from "./components/Navigation";
+import Frontpage from "./components/Frontpage";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <Navigation />
+                <Frontpage />
             </header>
         </div>
     );
